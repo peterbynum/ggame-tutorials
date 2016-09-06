17da0d8444f66d0975bf598dab53c480f92ba4b0
peterbynum <peterbynum@hanovernorwichschools.org>
2016-09-06 14:01:24 -0400

    Updated from Brython Server: September 6, 2016 2:01:24 PM EDT

--- tutorial3.py
+++ tutorial3.py
@@ -1,2 +1,62 @@
-#Peter Bynum
-#Assets and Looping Tutorial
+"""
+fizzbuzz.py
+Author: Peter Bynum
+Credit: top answer in http://stackoverflow.com/questions/4432208/how-does-work-in-python
+
+Assignment: FizzBuzz
+
+Write a program that prints the numbers from 1 to 100. But for 
+multiples of three print “Fizz” instead of the number and for 
+the multiples of five print “Buzz”. For numbers which are multiples 
+of both three and five print “FizzBuzz”.
+
+We will use a variation of this test in which the last number of 
+the series isn't necessarily 100, and the two numbers being tested 
+for multiples aren't necessarily three and five. For example, your 
+program should behave just like this:
+
+How many numbers shall we print? 25
+For multiples of what number shall we print 'Fizz'? 3
+For multiples of what number shall we print 'Buzz'? 5
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+Fizz
+22
+23
+Fizz
+Buzz
+"""
+
+num = int(input("How many numbers shall we print? "))
+m1 = int(input("For multiples of what number shall we print 'Fizz'? "))
+m2 = int(input("For multiples of what number shall we print 'Buzz'? "))
+
+for n in range (1, num+1):
+    if n%(m1*m2)==0:
+        print("FizzBuzz")
+    elif n%m1==0:
+        print("Fizz")
+    elif n%m2==0:
+        print("Buzz")
+    else:
+        print(n)
+
+        
